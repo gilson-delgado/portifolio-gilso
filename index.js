4855569cc169e0aca34ac17cb63ce629354793a9
gilson-delgado <gcdelgaa@gmail.com>
2024-01-22 16:29:56 -0300

    adicionado index.js, adiconado @import font roboto e poppins

---
+++ index.js
@@ -0,0 +1,5 @@
+myFunction();
+            function myFunction(){
+            let myName = "(Gilson Delgado / Desenvolvedor Front-End.)";
+            document.getElementById("myName").innerHTML =  " " + myName;
+}
